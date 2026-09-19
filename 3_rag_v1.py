75f079965a63fcb197afd6bc112c567dbcbd3943
foysalpranto121 <foysalpranto2002@gmail.com>
2026-09-19 10:34:28 +0600

    added rag v1

--- 3_rag_v1.py
+++ 3_rag_v1.py
@@ -10,6 +10,9 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnableParallel, RunnablePassthrough, RunnableLambda
 from langchain_core.output_parsers import StrOutputParser
 
+os.environ['LANGCHAIN_PROJECT'] = 'rag_v1 LLM APP'
+
+
 load_dotenv()  # expects OPENAI_API_KEY in .env
 
 PDF_PATH = "islr.pdf"  # <-- change to your PDF filename
